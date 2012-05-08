3f6d5dbdd1fc05cfa043388e7c40d576e37321e2
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-05-08 15:18:46 -0400

    Added growls index

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '3.2.3'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
+gem 'rspec'
+gem 'capybara'
 
 
 # Gems used only for assets and not required
--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Hungrlr::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'messages#index'
+  root :to => 'growls#index'
 
   # See how all your routes lay out with "rake routes"
 
