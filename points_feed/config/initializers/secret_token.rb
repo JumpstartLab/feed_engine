cb2941b2873c0a13297e7ce51f8bb78a31e5b419
Nisarg Shah <nisargshah100@gmail.com>
2012-05-08 13:57:23 -0400

    initial commit

---
+++ points_feed/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PointsFeed::Application.config.secret_token = '8876cf51d9cb9baaf571e053a521261aadf9f3edad061a6827703c72a93eeef7940bbcdfde7b2413bde53c6efb0afffd9d3687a529abb36f840367f45b98bccf'
