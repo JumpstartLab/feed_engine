51c73fcb5fa024839c36c45cd34964f374e83135
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-05-08 17:07:36 -0400

    adding validation

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,11 +2,14 @@ class Image < Growl
   # attr_accessible :title, :body
   validates_presence_of :link, message: "You must provide a link to an image."
   validate :check_image_link
+  validates_length_of :comment, :within => 3..256
   IMAGE_VALIDATOR_REGEX = "^https?:\/\/(?:[a-z\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpg|gif|png)$"
 
   def check_image_link
-    unless link.match(IMAGE_VALIDATOR_REGEX)
-      errors.add(:link, "Given URL does not meet a valid image format.")
+    if link.match(IMAGE_VALIDATOR_REGEX)
+      errors.add(:link, "URL must start with http and be a .jpg, .gif, or .png")
+    elsif link.size > 2048
+      errors.add(:link, "Given URL needs to be less then 2048 characters")
     end
   end
 
