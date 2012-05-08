5875d77d589885b50719a30441e551b536708843
Nisarg Shah <nisargshah100@gmail.com>
2012-05-08 16:25:22 -0400

    added bootstrap, devise configured & rspec added

---
+++ points_feed/app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :display_name
+
+  validates :display_name, :presence => true
+
+  # attr_accessible :title, :body
+end
