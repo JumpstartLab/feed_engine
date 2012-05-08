3f72063ad696f9907567c4b8b231e198c97d0e03
Darrell Rivera <darrell.rivera@livingsocial.com>
2012-05-08 18:07:29 -0400

    Add simplecov and refactored user request spec

--- points_feed/spec/requests/user_requests_spec.rb
+++ points_feed/spec/requests/user_requests_spec.rb
@@ -5,17 +5,18 @@ describe User do
     context "and when I visit the signup path" do
       before(:each) do
         visit signup_path
+        fill_in "Email", :with => "foo@bar.com"
+        fill_in "Display name", :with => "displayname"
+        fill_in "Password", :with => "hungry"
+        fill_in "Password confirmation", :with => "hungry"
       end
+
       it "has the signup form" do
         page.should have_content("Sign up")
       end
 
       context "and when I fill out information properly" do
         it "should show confirmation message" do
-          fill_in "Email", :with => "foo@bar.com"
-          fill_in "Display name", :with => "displayname"
-          fill_in "Password", :with => "hungry"
-          fill_in "Password confirmation", :with => "hungry"
           click_button "Sign Up"
           page.should have_content("Welcome! You have signed up successfully.")
         end
@@ -28,53 +29,36 @@ describe User do
       context "and when I fill out the information improperly" do
         it "should show an error message for improper email format" do
           fill_in "Email", :with => "foo@"
-          fill_in "Display name", :with => "displayname"
-          fill_in "Password", :with => "hungry"
-          fill_in "Password confirmation", :with => "hungry"
           click_button "Sign Up"
           page.should have_content("is invalid")
         end
 
         it "should show an error message for a blank email" do
           fill_in "Email", :with => ""
-          fill_in "Display name", :with => "displayname"
-          fill_in "Password", :with => "hungry"
-          fill_in "Password confirmation", :with => "hungry"
           click_button "Sign Up"
           page.should have_content("can't be blank")
         end
 
         it "should show an error message for improper display name" do
           pending
-          fill_in "Email", :with => "foo@bar.com"
           fill_in "Display name", :with => "display name"
-          fill_in "Password", :with => "hungry"
-          fill_in "Password confirmation", :with => "hungry"
           click_button "Sign Up"
           page.should have_content("can't be blank")
         end   
 
         it "should show an error message for a blank display name" do
-          fill_in "Email", :with => "foo@bar.com"
           fill_in "Display name", :with => ""
-          fill_in "Password", :with => "hungry"
-          fill_in "Password confirmation", :with => "hungry"
           click_button "Sign Up"
           page.should have_content("can't be blank")
         end 
 
         it "should show an error message for a blank password" do
-          fill_in "Email", :with => "foo@bar.com"
-          fill_in "Display name", :with => "displayname"
           fill_in "Password", :with => ""
-          fill_in "Password confirmation", :with => "hungry"
           click_button "Sign Up"
           page.should have_content("can't be blank")
         end  
 
         it "should show an error message for a password less than 6 characters" do
-          fill_in "Email", :with => "foo@bar.com"
-          fill_in "Display name", :with => "displayname"
           fill_in "Password", :with => "12345"
           fill_in "Password confirmation", :with => "12345"
           click_button "Sign Up"
@@ -82,8 +66,6 @@ describe User do
         end   
 
         it "should show an error message for not matching passwords" do
-          fill_in "Email", :with => "foo@bar.com"
-          fill_in "Display name", :with => "displayname"
           fill_in "Password", :with => "123456"
           fill_in "Password confirmation", :with => "1234567"
           click_button "Sign Up"
@@ -104,8 +86,6 @@ describe User do
           page.should have_content("Sign up")
           find_field('Display name').value.should == user.display_name
         end
-
-
       end
     end  
   end
