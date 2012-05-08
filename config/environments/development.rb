3f12a56cdabacebc0e5e4d596079cd694b80f1b8
Charles Strahan <charles.c.strahan@gmail.com>
2012-05-08 15:23:40 -0400

    Use slim templates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ FeedEngine::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Pretty print html
+  Slim::Engine.set_default_options :pretty => true
 end
