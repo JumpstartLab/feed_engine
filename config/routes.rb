c3748263eef2b28d36274fd83436abbc5b9b7ddc
ADigitalNative <adigitalnative@gmail.com>
2012-05-08 16:49:53 -0400

    Added basic Devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Hungrlr::Application.routes.draw do
 
   resources :growls, :images, :links, :messages
   resource :dashboard
+  devise_for :users
+
+>>>>>>> Added basic Devise
   root :to => 'growls#index'
 end
