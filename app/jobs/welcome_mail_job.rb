2fc4e7d30cfcee92dd31a0e4233045240bc864dd
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-05-09 18:57:39 -0400

    Pull out id from json in mailer job

--- app/jobs/welcome_mail_job.rb
+++ app/jobs/welcome_mail_job.rb
@@ -2,7 +2,7 @@ class WelcomeMailJob
   @queue = :emails
 
   def self.perform(user)
-    user = User.find(user[:id])
+    user = User.find(user["id"])
     UserMailer.welcome_email(user).deliver
   end
 end
