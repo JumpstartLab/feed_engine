fdc357a6444d3ba7fd6c54c6d610781e987a3995
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-05-09 14:58:29 -0400

    Working on routing

--- app/controllers/growls_controller.rb
+++ app/controllers/growls_controller.rb
@@ -2,4 +2,8 @@ class GrowlsController < ApplicationController
   def index
     @growls = Growl.order("created_at DESC").page(params[:page])
   end
+
+  def show
+    @growls = Growl.find_by_subdomain!(request.subdomain)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Hungrlr::Application.routes.draw do
   resources :growls, :images, :links, :messages
   resource :dashboard
 
-
+  match '/' => 'growls#show', :constraints => { :subdomain => /.+/ }
   root :to => 'growls#index'
 end
