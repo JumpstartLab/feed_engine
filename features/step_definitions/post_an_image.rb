853400e4d954e38d4cee148e1e7981ca831e0060
michael verdi <michael.v.verdi@gmail.com>
2012-05-09 14:12:55 -0400

    setting up image post

---
+++ features/step_definitions/post_an_image.rb
@@ -0,0 +1,51 @@
+Given /^I click the "(.*?)" tab$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see a form to create an image message$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see an error message requiring a link to an image$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see the form to create an image message$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I fill in the image link field with a link of (\d+) characters$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see an error message requiring the image link to be less than or equal to (\d+) characters$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I fill in the link field with "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see an error message requiring the link format to look like an http\/https link$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I fill in the link field with "(.*?)";$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see an error message requiring the link format to end in an image extension \(car\-insensitive: jpg, jpeg, gif, bmp, png\)$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I fill in the comment field with (\d+) 'a's$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see an error message requiring the comment to be less than or equal to (\d+) characters$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I fill in the comment field with "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
