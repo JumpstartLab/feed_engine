756995d3edd3270471f1a18db09c78835212f519
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-05-09 15:40:37 -0400

    Added /signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 FeedEngine::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users 
+
+  devise_scope :user do 
+    get "signup" => "devise/registrations#new", :as => :new_user
+  end
+
 
   root :to => "home#index"
 
