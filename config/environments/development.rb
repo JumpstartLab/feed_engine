f68e1269205112bc2b6080a9cb5ee2f3b3f7d9d8
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-05-09 13:53:28 -0400

    Install Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,8 @@ FeedEngine::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
+  # mailer configs
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
