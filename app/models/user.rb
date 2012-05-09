f61b557c539a48ef2a88171a05b9d9caa1992746
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-05-09 16:09:37 -0400

    validate fields for sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :display_name, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  validates :display_name, :presence => true
+  validates :display_name, :format => { :with => /\A[a-zA-Z0-9_-]+\z/, 
+            :message => "may only contain letters, numbers, dashes, and underscores." }  
 end
