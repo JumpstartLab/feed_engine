ff8fbda10adb55fee8fc849ec95492f5b846edf2
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-05-09 15:08:33 -0400

    Added display name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :display_name, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 end
--- db/migrate/20120509175354_devise_create_users.rb
+++ db/migrate/20120509175354_devise_create_users.rb
@@ -3,6 +3,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
+      t.string :display_name,              :null => false, :default => "" 
       t.string :encrypted_password, :null => false, :default => ""
 
       ## Recoverable
