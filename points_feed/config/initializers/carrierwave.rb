91c7ae1b1a6d557832f309e4651326494b2c10a1
Melanie Gilman <melanie.gilman@gmail.com>
2012-05-10 15:01:32 -0400

    Modify Carrierwave initializer

--- points_feed/config/initializers/carrierwave.rb
+++ points_feed/config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS'       # required
-    # :aws_access_key_id      => 'xxx',       # required
-    # :aws_secret_access_key  => 'yyy',       # required
+    :provider               => 'AWS',       # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],       # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],       # required
     # :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'pointsfeed'                     # required
