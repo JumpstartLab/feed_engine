1d21bb4f2704eab04e6c324bfa8a7d12af80c1d2
athal7 <andrew.thal@livingocial.com>
2012-05-10 14:24:22 -0400

    make posts also show on root

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
     # if logged_in
     #   redirect_to posts_path
     # end
+    @user = current_user
+    @posts = Kaminari.paginate_array(@user.sorted_posts).page(params[:page]).per(12)
   end
 end
