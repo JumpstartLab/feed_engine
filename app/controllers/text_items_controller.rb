1f0287212afb32b6ddbf0c759b4b350ac672e3ac
Chris Anderson <christopher.anderson@gmail.com>
2012-05-10 10:53:31 -0400

    I AM ALL THAT IS MAN

--- app/controllers/text_items_controller.rb
+++ app/controllers/text_items_controller.rb
@@ -1,4 +1,5 @@
 class TextItemsController < ApplicationController
+
   def new
     @text_item = TextItem.new
   end
@@ -7,11 +8,21 @@ class TextItemsController < ApplicationController
     @text_item = TextItem.new(params[:text_item])
 
     if @text_item.save
-      redirect_to dashboard_path, notice: 'Post was successfully created.'
+      redirect_to dashboard_path, notice: 'Link was successfully created.'
     else
       @link_item = LinkItem.new
       @image_item = ImageItem.new
       render :template => "dashboard/show"
     end
+    # respond_to do |format|
+    #   if @text_item.save
+    #     format.html { redirect_to dashboard_path, notice: 'Post was successfully created.' }
+    #     format.js 
+    #   else
+    #     # format.html { render action: "new" }
+    #     format.json { render json: @link_item.errors, status: :unprocessable_entity }
+    #     format.js { render 'reload' }
+    #   end
+    # end
   end
 end
