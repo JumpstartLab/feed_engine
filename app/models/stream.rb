ab9609e1c66642b8ce95f90b23dc8b02bfba403b
Austen Ito <austen.services@gmail.com>
2012-05-10 11:26:52 -0400

    Fixed load errors

--- app/models/stream.rb
+++
@@ -1,4 +0,0 @@
-class Stream < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :stream_item, :polymorhpic => true
-end
--- spec/models/stream_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Stream do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
