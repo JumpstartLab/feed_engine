e076580c761a6d34114f7c41dcbe239571dd517e
Edward Weng <edweng@gmail.com>
2012-05-10 14:02:46 -0400

    Added user_home paths

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,4 @@
 class DashboardsController < ApplicationController
   def show
-    @growls = Growl.by_date.page(params[:page])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,28 @@
 Hungrlr::Application.routes.draw do
 
-  #constraints(NoSubdomain) do
-    match "/home" => "pages#home"
+  # constraints(NoSubdomain) do
+  match "/home" => "pages#home"
 
-    devise_for :users
+  devise_for :users
 
-    devise_scope :user do
-      get '/signup' => 'devise/registrations#new'
-      get '/login' => 'devise/sessions#new'
-    end
+  devise_scope :user do
+    get '/signup' => 'devise/registrations#new'
+    get '/login' => 'devise/sessions#new'
+  end
 
-    resources :growls, :images, :links, :messages
-    resource :dashboard
+  resources :growls, :images, :links, :messages
+  resource :dashboard
 
-    namespace :api do
-        namespace :v1 do
-          resources :images
-          resources :meta_data
-        end
-    end
+  namespace :api do
+      namespace :v1 do
+        resources :images
+        resources :meta_data
+      end
+  end
+
+  constraints(NoSubdomain) do  
     root :to => 'pages#home'
-  #end
+  end
 
   constraints(Subdomain) do
     match '/' => 'growls#show'
