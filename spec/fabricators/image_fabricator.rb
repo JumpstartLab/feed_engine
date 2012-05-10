bb3f90d8772f546e029c71ff6bce61e6ff6e1f29
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-10 15:34:17 -0400

    Fix image per Yoho

--- spec/fabricators/image_fabricator.rb
+++ spec/fabricators/image_fabricator.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: images
+#
+#  id          :integer         not null, primary key
+#  description :text
+#  url         :text
+#  poster_id   :integer
+#  created_at  :datetime        not null
+#  updated_at  :datetime        not null
+#
+
 Fabricator(:image) do
   description  Faker::Lorem.paragraph(word_count = 3)
   url          "http://image.gsfc.nasa.gov/image/image_launch_a5.jpg"
--- spec/fabricators/message_fabricator.rb
+++ spec/fabricators/message_fabricator.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer         not null, primary key
+#  body       :text
+#  poster_id  :integer
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 Fabricator(:message) do
   body  Faker::Lorem.paragraph(word_count = 10)
   poster_id 1
