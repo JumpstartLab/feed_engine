38631fc407ceb07fdb15a5fc9b36a64bae1354df
Melanie Gilman <melanie.gilman@gmail.com>
2012-05-10 13:51:37 -0400

    Add AWS and fog

--- points_feed/app/uploaders/file_uploader.rb
+++ points_feed/app/uploaders/file_uploader.rb
@@ -11,13 +11,13 @@ class FileUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
---
+++ points_feed/config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS'       # required
+    # :aws_access_key_id      => 'xxx',       # required
+    # :aws_secret_access_key  => 'yyy',       # required
+    # :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'pointsfeed'                     # required
+  # config.fog_host       = 'https://s3.amazonaws.com'            # optional, defaults to nil
+  config.fog_public     = true                                   # optional, defaults to true
+  # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
