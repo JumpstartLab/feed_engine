dc3d15c879a132fc8ef261be94acede5537ee92c
athal7 <andrew.thal@livingocial.com>
2012-05-09 20:09:11 -0400

    Add assets precompile task for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ FeedEngine::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  #
+  RAILS_ENV=production bundle exec rake assets:precompile
 end
