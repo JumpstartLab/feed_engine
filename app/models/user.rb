cff80225f6c5c542bf2bdf0f5b79b574fa7c5f39
Jonan Scheffler <jonanscheffler@gmail.com>
2012-05-10 11:21:09 -0400

    Change validations for consistency.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,12 @@ class User < ActiveRecord::Base
   has_secure_password
 
   attr_accessible :email, :password, :password_confirmation, :display_name
-  validates :email, :uniqueness => true, :presence => true
+  validates :email, :uniqueness => true
+  validates_presence_of :email, :message => "Email is required."
   validates :password, :length => { :minimum => 6 }
-  validates_format_of :email, :with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i
+  validates_format_of :email,
+                      :with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i,
+                      :allow_nil => true
   validates :display_name, :presence => true,
                            :format => { :with => /^[a-zA-Z\d\-_]*$/,
     :message => "must be only letters, numbers, dashes, or underscores"
