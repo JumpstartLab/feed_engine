f0e10d711a9e49e2858d591c6ddce4ff9d8f8795
Chris Anderson <christopher.anderson@gmail.com>
2012-05-10 12:28:53 -0400

    switched image url to text from string

--- db/migrate/20120510001222_create_image_items.rb
+++ db/migrate/20120510001222_create_image_items.rb
@@ -1,7 +1,7 @@
 class CreateImageItems < ActiveRecord::Migration
   def change
     create_table :image_items do |t|
-      t.string :url
+      t.text :url
       t.string :comment
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(:version => 20120510131552) do
 
   create_table "image_items", :force => true do |t|
-    t.string   "url"
+    t.text     "url"
     t.string   "comment"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
