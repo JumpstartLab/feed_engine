3083f3b47361c2707bf7edc5850bc5fcbd266656
Melanie Gilman <melanie.gilman@gmail.com>
2012-05-10 12:05:05 -0400

    Add uploader & custom validations for IMagePost

--- points_feed/app/models/post.rb
+++ points_feed/app/models/post.rb
@@ -1,10 +1,16 @@
 class Post < ActiveRecord::Base
-  attr_accessible :comment, :title, :content, :type
+  attr_accessible :comment, :title, :content, :type, :file
   belongs_to :user
+  mount_uploader :file, FileUploader
   
   validates_presence_of :user_id
   validates :comment, :length => { :maximum => 256 }
-  validates_presence_of :content
+  validates_presence_of :content, :if => :validate_presence_of_content?
+  
+  def validate_presence_of_content?
+    true
+  end
+
 
   default_scope :order => 'created_at DESC'
 
--- points_feed/db/schema.rb
+++ points_feed/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120510133132) do
+ActiveRecord::Schema.define(:version => 20120510151229) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20120510133132) do
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "file"
   end
 
   add_index "posts", ["id"], :name => "index_posts_on_id"
