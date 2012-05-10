ca62ae0951cf428f6b68431aa4ed67d980ee6489
athal7 <andrew.thal@livingocial.com>
2012-05-10 14:25:20 -0400

    Only look for posts when logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,9 @@ class HomeController < ApplicationController
     # if logged_in
     #   redirect_to posts_path
     # end
-    @user = current_user
-    @posts = Kaminari.paginate_array(@user.sorted_posts).page(params[:page]).per(12)
+    if current_user
+      @user = current_user
+      @posts = Kaminari.paginate_array(@user.sorted_posts).page(params[:page]).per(12)
+    end
   end
 end
