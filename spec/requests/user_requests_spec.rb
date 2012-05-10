571721f71cd14027acec91a8c4f8ea2f24774871
Jonan Scheffler <jonanscheffler@gmail.com>
2012-05-10 10:16:23 -0400

    Edit user tests.

--- spec/requests/user_requests_spec.rb
+++ spec/requests/user_requests_spec.rb
@@ -1,12 +1,44 @@
 require 'spec_helper'
 
 describe User do
+  let(:user) { Fabricate(:user) }
+
   context "who is authenticated" do
+    describe "and visits the dashboard account tab" do
+      before(:each) do
+        visit dashboard_path
+        click_link "Account"
+      end
+
+      it "sees a form to change password" do
+        page.should have_content "Password"
+        page.should have_content "Password confirmation"
+      end
+
+      it "can change their password with valid input" do
+        click_button 'Change password'
+        user.password = 'foobar'
+        fill_password_change_form_for(user)
+        click_link "Log out"
+        login_as(user)
+        page.should have_content "Logged in."
+        current_path.should == dashboard_path
+      end
+
+      it "cannot change their password with invalid input" do
+        click_button 'Change password'
+        user.password = '123'
+        fill_password_change_form_for(user)
+        click_link "Log out"
+        login_as(user)
+        page.should_not have_content "Logged in."
+        current_path.should == dashboard_path
+      end
+    end
   end
 
   context "who is unauthenticated" do
     let(:new_user) { Fabricate.build(:user) }
-    let(:user) { Fabricate(:user) }
 
     it "can sign up from the home page" do
       visit root_path
@@ -25,56 +57,69 @@ describe User do
         page.should have_content "Password"
       end
 
-      it "can create an account" do
-        expect { click_button "Sign Up" }.to change { User.count }.by(1)
+      describe "when signing up" do
+        it "is persisted" do
+          expect { click_button "Sign Up" }.to change { User.count }.by(1)
+        end
+
+        it "is redirected to their dashboard with a flash message" do
+          click_button "Sign Up"
+          current_path.should == dashboard_path
+          page.should have_content "Thank you for signing up!"
+        end
+
+        it "receives an email" do
+          expect { click_button "Sign Up" }.to change {
+            ActionMailer::Base.deliveries.length
+          }.by(1)
+        end
       end
 
-      it "is redirected to their dashboard after sign up with a flash message" do
-        click_button "Sign Up"
-        current_path.should == dashboard_path
-        page.should have_content "Thank you for signing up!"
-      end
+      describe "cannot sign up" do
+        it "with an empty email address" do
+          fill_in "Email", :with => ""
+          expect { click_button "Sign Up" }.to change { User.count }.by(0)
+          page.should have_content "Email can't be blank"
+        end
 
-      it "cannot sign up with an empty email address" do
-        fill_in "Email", :with => ""
-        expect { click_button "Sign Up" }.to change { User.count }.by(0)
-        page.should have_content "Email can't be blank"
-      end
-      it "cannot sign up with an already-used email address" do
-        fill_signup_form_as(user)
-        expect { click_button "Sign Up" }.to change { User.count }.by(0)
-        page.should have_content "Email has already been taken"
-      end
-      it "cannot sign up with an invalid email address" do
-        fill_in "Email", :with => "test"
-        expect { click_button "Sign Up" }.to change { User.count }.by(0)
-        page.should have_content "Email is invalid"
-      end
-      it "cannot sign up with an invalid display name" do
-        fill_in "Display name", :with => "test test"
-        expect { click_button "Sign Up" }.to change { User.count }.by(0)
-        page.should have_content "Display name must be only letters, numbers, dashes, or underscores"
-      end
-      it "cannot sign up with an empty password" do
-        fill_in "Password", :with => ""
-        fill_in "Password confirmation", :with => ""
-        expect { click_button "Sign Up" }.to change { User.count }.by(0)
-        page.should have_content "Password can't be blank"
-      end
-      it "cannot sign up with an inconsistent password and confirmation" do
-        fill_in "Password", :with => "testering"
-        fill_in "Password confirmation", :with => "tester"
-        expect { click_button "Sign Up" }.to change { User.count }.by(0)
-        page.should have_content "Password doesn't match confirmation"
-      end
-      it "cannot sign up with too short of a password" do
-        fill_in "Password", :with => "testr"
-        fill_in "Password confirmation", :with => "testr"
-        expect { click_button "Sign Up" }.to change { User.count }.by(0)
-        page.should have_content "Password is too short"
-      end
-      it "receives an email after a successful sign-up" do
-        expect { click_button "Sign Up" }.to change { ActionMailer::Base.deliveries.length }.by(1)
+        it "with an already-used email address" do
+          fill_signup_form_as(user)
+          expect { click_button "Sign Up" }.to change { User.count }.by(0)
+          page.should have_content "Email has already been taken"
+        end
+
+        it "with an invalid email address" do
+          fill_in "Email", :with => "test"
+          expect { click_button "Sign Up" }.to change { User.count }.by(0)
+          page.should have_content "Email is invalid"
+        end
+
+        it "with an invalid display name" do
+          fill_in "Display name", :with => "test test"
+          expect { click_button "Sign Up" }.to change { User.count }.by(0)
+          page.should have_content "Display name must be only letters, numbers, dashes, or underscores"
+        end
+
+        it "with an empty password" do
+          fill_in "Password", :with => ""
+          fill_in "Password confirmation", :with => ""
+          expect { click_button "Sign Up" }.to change { User.count }.by(0)
+          page.should have_content "Password can't be blank"
+        end
+
+        it "with an inconsistent password and confirmation" do
+          fill_in "Password", :with => "testering"
+          fill_in "Password confirmation", :with => "tester"
+          expect { click_button "Sign Up" }.to change { User.count }.by(0)
+          page.should have_content "Password doesn't match confirmation"
+        end
+
+        it "with too short of a password" do
+          fill_in "Password", :with => "testr"
+          fill_in "Password confirmation", :with => "testr"
+          expect { click_button "Sign Up" }.to change { User.count }.by(0)
+          page.should have_content "Password is too short"
+        end
       end
     end
   end
