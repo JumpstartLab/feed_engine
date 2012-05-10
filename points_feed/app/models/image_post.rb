e39f8931e2a952b80b7553e7d206ec5c9b3c66bb
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-05-09 20:36:34 -0400

    Refactor to meet technical particulars of tickets

--- points_feed/app/models/image_post.rb
+++ points_feed/app/models/image_post.rb
@@ -1,5 +1,10 @@
 class ImagePost < Post
   validates :content, :length => { :maximum => 2048 }
-  validates_format_of :content, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
-  validates_format_of :content, :with => /\.(png|jpg|jpeg|gif|bmp)$/
+  validates_format_of :content, 
+  :message => "for Image posts must contain a valid link",
+  :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
+
+  validates_format_of :content, 
+  :message => "for Image posts must contain a valid image",
+  :with => /\.(png|jpg|jpeg|gif|bmp)$/
 end
