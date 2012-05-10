74e7055a7dc27cb4786f51a0a082115de7eb004a
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-10 10:05:57 -0400

    fixes per PT stories, corrected double validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,18 +15,16 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation, :display_name
   validates :email, :uniqueness => true, :presence => true
-  validates_presence_of :password, :message => "Password can't be blank"
+  validates_presence_of :password, :message => "can't be blank"
   validates :password, :length => { :minimum => 6 }
   validates_format_of :email, :with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i
-  validates_format_of :display_name, :with => /^[a-zA-Z\d\-_]*$/,
-    :message => "Display name must be only letters,
-    numbers, dashes, or underscores"
-
+  validates :display_name, :presence => true,
+                           :format => { :with => /^[a-zA-Z\d\-_]*$/,
+    :message => "must be only letters, numbers, dashes, or underscores"
+                           } 
 
   def send_welcome_email
     UserMailer.signup_notification(self).deliver
   end
-
-  validates :email, :uniqueness => true
 
 end
