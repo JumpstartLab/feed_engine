9f3b4a075732acbe33b347096dd350326ce971e1
athal7 <andrew.thal@livingocial.com>
2012-05-10 15:33:55 -0400

    add mail body to sign up notification

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,11 +27,19 @@ FeedEngine::Application.configure do
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
+config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
   config.assets.compress = false
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  ActionMailer::Base.smtp_settings = {
+   :address  => "smtp.gmail.com",
+   :port  => 587,
+   :user_name  => "hungryopensource",
+   :password  => "hungryacademy",
+   :authentication  => "plain"
+ }
 end
