e7fefdeff77bce031d7570f829ab63fcbd6c691f
Jacqueline Chenault <adigitalnative+adigitalnative@gmail.com>
2012-05-10 17:58:02 -0400

    Continuing to switch over to display_nanme

--- app/controllers/growls_controller.rb
+++ app/controllers/growls_controller.rb
@@ -1,7 +1,7 @@
 class GrowlsController < ApplicationController
   def show
     subdomain = request.subdomain
-    @user = User.where{username.matches subdomain}.first
+    @user = User.where{display_name.matches subdomain}.first
 
     @growls = @user.growls.by_date.page(params[:page]) if @user
   end
