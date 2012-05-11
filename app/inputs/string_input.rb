c95f9bb6722cc1b10d946e20cabb7a7fd95d4555
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-05-10 21:29:59 -0400

    passing tests

--- app/inputs/string_input.rb
+++ app/inputs/string_input.rb
@@ -1,5 +1,6 @@
 class StringInput < SimpleForm::Inputs::Base
   def input
-    "#{@builder.text_field(attribute_name, {:class => 'medium input-text'})}".html_safe
+    input_html_options[:class] << "medium input-text"
+    "#{@builder.text_field(attribute_name, input_html_options)}".html_safe
   end
 end
