e20ef27d0043789b76cfcae1f6e271dc0415e0bf
Chris Maddox <tyre77@gmail.com>
2012-05-10 20:27:19 -0400

    scss with Andy :)

--- config/initializers/pagination.rb
+++ config/initializers/pagination.rb
@@ -1,10 +1,17 @@
 class Array
-  @@per_page_default = 10
+  @@per_page_default = 12
 
   def page(page_num, per_page = @@per_page_default)
-    start_item = page_num * per_page
-    start_item = -10 if self[start_item] == nil
-    self.slice(start_item, per_page -1) 
+    unless (page_num * per_page) > self.length
+      start_item = page_num * per_page
+      unless (start_item + per_page) > self.length
+        self.slice(start_item, per_page)
+      else
+        self.slice(start_item..-1)
+      end
+    else
+      []
+    end
   end
 
 end
