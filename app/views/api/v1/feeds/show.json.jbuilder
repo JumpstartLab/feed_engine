d8d7a65bbe7967c307678b5f403532267eb0eca0
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-05-13 10:49:12 -0400

    Still working on tests

--- app/views/api/v1/feeds/show.json.jbuilder
+++ app/views/api/v1/feeds/show.json.jbuilder
@@ -3,6 +3,6 @@ json.id @user.id
 json.private @user.private
 json.link @user.api_link(request)
 json.items do |json|
-
+  json.growls @user.growls
 end
 json.web_url @user.web_url(request)
--- spec/apis/v1/feeds_spec.rb
+++ spec/apis/v1/feeds_spec.rb
@@ -6,7 +6,7 @@ describe 'api/v1/feed', type: :api do
   let!(:other_user) { FactoryGirl.create(:user_with_growls) }
   
   context "growls viewable by this user" do
-    let(:url) { "http://api.hungrlr.com/v1/feeds/#{user.display_name}" }
+    let(:url) { "http://api.hungrlr.dev/v1/feeds/#{user.display_name}" }
     before(:each) do
       get "#{url}.json"
     end
@@ -15,8 +15,8 @@ describe 'api/v1/feed', type: :api do
         last_response.status.should == 200
       end
       it "returns the specified users last 3 growls" do
-        growls_json = user.growls.as_json
-        JSON.parse(last_response.body)["most_recent"].should == growls_json
+        growls_json = user.growls.to_json
+        JSON.parse(last_response.body)["items"].should == growls_json
       end
     end
   end
