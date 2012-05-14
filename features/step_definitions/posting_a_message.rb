fe9c17ae794d2fdee9967436f67e9ded9b945f14
michael verdi <michael.v.verdi@gmail.com>
2012-05-14 14:55:07 -0400

    styling stream

--- features/step_definitions/posting_a_message.rb
+++ features/step_definitions/posting_a_message.rb
@@ -29,9 +29,7 @@ Then /^I should see an error message requiring text to be less than or equal to 
 end
 
 Then /^the data I have entered is present$/ do
-  save_and_open_page
   page.should have_content @message
-  # find('input[name="text_post[body]"]').value.should include @message
 end
 
 When /^I fill in the message text with "(.*?)"$/ do |text|
