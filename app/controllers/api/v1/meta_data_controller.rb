d6ecb5f99972589dd54b534856d5f9e8b9d96779
ADigitalNative <adigitalnative@gmail.com>
2012-05-14 17:29:19 -0400

    Added api check to meta_data_controller

--- app/controllers/api/v1/meta_data_controller.rb
+++ app/controllers/api/v1/meta_data_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::MetaDataController < ApplicationController
+    before_filter :authenticate_user
+
   def create
     render :json => MetaData.find_link_data(params[:url])
+  end
+
+  private
+
+  def authenticate_user
+    @current_user = User.find_by_authentication_token(params[:token])
+    render :json => "Token is invalid.".to_json unless @current_user
+  end
+
+  def current_user
+    @current_user
   end
 end
