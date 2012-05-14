3c51d4a178eb26cde5f62bcd2be515c56eab0854
Chris Maddox <tyre77@gmail.com>
2012-05-14 18:15:57 -0400

    unicorn again

--- config/initializers/unicorn.rb
+++ config/initializers/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/deployer/apps/feed_engine/current"
+root = "/home/deploy/apps/feed_engine/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
