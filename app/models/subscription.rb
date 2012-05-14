2a254d944f8b09cbd79cd4c19038e09a9a5907ab
Jonan Scheffler <jonanscheffler@gmail.com>
2012-05-14 14:57:34 -0400

    Annotated subscription.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: subscriptions
+#
+#  id         :integer         not null, primary key
+#  provider   :string(255)
+#  uid        :string(255)
+#  user_name  :string(255)
+#  user_id    :integer
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 class Subscription < ActiveRecord::Base
   attr_accessible :user_name, :provider, :uid, :user_id
 
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: subscriptions
+#
+#  id         :integer         not null, primary key
+#  provider   :string(255)
+#  uid        :string(255)
+#  user_name  :string(255)
+#  user_id    :integer
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 require 'spec_helper'
 
 describe Subscription do
