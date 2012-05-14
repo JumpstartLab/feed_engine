f6ce057759a3beede691ea20644c68d10998b500
Edward Weng <edweng@gmail.com>
2012-05-14 18:14:40 -0400

    feeds spec

--- spec/apis/v1/feeds_spec.rb
+++ spec/apis/v1/feeds_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe 'api/v1/feed', type: :api do
   let!(:user) { FactoryGirl.create(:user_with_growls) }
-  let!(:token) { user.authentication_token }
   let!(:other_user) { FactoryGirl.create(:user_with_growls) }
   
   context "growls viewable by this user" do
-    let(:url) { "http://api.hungrlr.com/v1/feeds/#{user.display_name}" }
+    let(:url) { "http://api.hungrlr.dev/v1/feeds/#{user.display_name}" }
     before(:each) do
       get "#{url}.json"
     end
@@ -23,13 +22,23 @@ describe 'api/v1/feed', type: :api do
   end
 
   context "creating growls through the api" do
-    let(:url) { "http://api.hungrlr.com/v1/feeds/#{user.display_name}" }
+    let(:message) { FactoryGirl.build(:message) }
+    let(:url) { "http://api.hungrlr.dev/v1/feeds/#{user.display_name}" }
     before(:each) do
-      post "#{url}.json"
     end
-    describe "json" do
+
+    describe "when valid parameters are passed in" do
       it "returns a successful response" do
-        pending
+        post "#{url}.json", token: user.authentication_token, body: { type: "Message", comment: message.comment }
+        last_response.status.should == 201
+      end
+    end
+
+    describe "when invalid parameters are passed in" do
+      it "returns a unsuccessful response" do
+        post "#{url}.json", token: user.authentication_token, body: { type: "Message" }
+        last_response.status.should == 406
+        last_response.body.should =~ /"You must provide a message."/
       end
     end
   end
