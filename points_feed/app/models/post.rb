620793d722647427d46ad11317d43b4111db421f
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-05-15 14:26:48 -0400

    Add refeed option to Post

--- points_feed/app/models/post.rb
+++ points_feed/app/models/post.rb
@@ -1,17 +1,30 @@
 class Post < ActiveRecord::Base
-  attr_accessible :comment, :content, :type, :file
+  attr_accessible :comment, :content, :type, :file, :original_post_id
   belongs_to :user
   mount_uploader :file, FileUploader
   
   validates_presence_of :user_id
   validates :comment, :length => { :maximum => 256 }
 
+  after_create :set_original_post_id
 
   default_scope :order => 'created_at DESC'
+
+  def set_original_post_id
+    update_attribute(:original_post_id, self.id) unless self.original_post_id
+  end
 
   def posted_at
     self.created_at
   end
+
+  def refeed(user)
+    refeeded_post = self.dup
+    user.posts << refeeded_post
+    refeeded_post.update_attribute(:original_post_id, self.original_post_id)
+  end
+
+
 
   # def self.class_for(type)
   #   type.to_s.constantize rescue TextPost
--- points_feed/db/schema.rb
+++ points_feed/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120515011351) do
+ActiveRecord::Schema.define(:version => 20120515181422) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -44,9 +44,10 @@ ActiveRecord::Schema.define(:version => 20120515011351) do
     t.text     "comment"
     t.string   "type"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
     t.string   "file"
+    t.integer  "original_post_id"
   end
 
   add_index "posts", ["id"], :name => "index_posts_on_id"
