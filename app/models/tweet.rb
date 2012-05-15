30275c84bb1e2c3982f385a15cd36569a6750f95
Andrewglass1 <Andrewglass1@gmail.com>
2012-05-15 18:02:08 -0400

    in progress

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,17 @@
 class Tweet < ActiveRecord::Base
   include Post
   attr_accessible :source_id, :handle, :tweet_time
+
+
+#pulling in refactor from user
+
+  def self.import_tweets(user_id)
+    user = User.find(user_id)
+    params = {:user_id => user.twitter_id, :count=>200}
+    params[:since_id] = user.tweets.last.source_id if user.tweets.any?
+    Twitter.user_timeline(params).reverse.each do |tweet|
+      user.tweets.create(content: tweet.text, source_id: tweet.id, handle: tweet.user.screen_name, tweet_time: tweet.created_at)
+    end
+  end
+
 end
