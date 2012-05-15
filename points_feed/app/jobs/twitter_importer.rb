caf467cfeeae500884b51176f084e709d6b33a74
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-05-14 21:05:45 -0400

    Begin fleshing out twitter feed import

--- points_feed/app/jobs/twitter_importer.rb
+++ points_feed/app/jobs/twitter_importer.rb
@@ -2,6 +2,20 @@ class TwitterImporter
   @queue = :medium
 
   def self.perform
-    puts "This is me, pretending to import all the twitters"
+    users_with_twitter.each do |user|
+      import_tweets(user)
+    end
+  end
+
+  private
+
+  def users_with_twitter
+    User.where("twitter_name IS NOT null")
+  end
+
+  def import_tweets(user)
+    Twitter.user_timeline(user.twitter_name).each do |tweet|
+
+    end
   end
 end
