e4755333c7d83b3a50be68a1eb3c69d41c68e586
Nisarg Shah <nisargshah100@gmail.com>
2012-05-15 10:54:33 -0400

    fix links

--- points_feed/app/decorators/link_post_decorator.rb
+++ points_feed/app/decorators/link_post_decorator.rb
@@ -2,7 +2,7 @@ class LinkPostDecorator < ApplicationDecorator
   decorates :link_post
 
   def url
-    "http://api.pointsfeed.in/feeds/#{model.display_name}"
+    "http://api.pointsfeed.in/feeds/#{model.user.display_name}"
   end
 
   def as_json(*params)
