1f2620f7788349414457589eff12149055600efb
athal7 <andrew.thal@livingocial.com>
2012-05-15 17:22:48 -0400

    Implement github account connection

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,9 +5,19 @@ class SubscriptionsController < ApplicationController
 
   def create
     Subscription.create_with_omniauth(request.env["omniauth.auth"], current_user)
-    notice = "Your account has been linked with twitter"
-    redirect_to dashboard_path, notice: notice
+    provider = request.env["omniauth.auth"]["provider"]
+    notice = "Your account has been linked with #{provider}"
+    if current_user.subscriptions.count < num_subscriptions
+      redirect_to new_subscription_path, notice: notice
+    else
+      redirect_to dashboard_path, notice: notice
+    end
   end
 
+  private
+
+  def num_subscriptions
+    2
+  end
 
 end
