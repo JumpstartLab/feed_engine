e2a822c89d30ad4abfbfbad0ce956a79d3d38c6d
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-05-15 14:51:02 -0400

    Removed get tweets from dashboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,6 @@ class DashboardsController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    Resque.enqueue(PullTwitterFeed)
     flash[:notice] = params[:message] if params[:message]
     @growl  = current_user.growls.build
     @growls = current_user.growls.by_date.page(params[:page])
