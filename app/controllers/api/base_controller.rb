593671804bcd4ebe262f2e1441edcdf530647f87
Charles Strahan <charles.c.strahan@gmail.com>
2012-05-15 17:44:58 -0400

    Progress on testing api

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -13,8 +13,8 @@ class Api::BaseController < ActionController::Base
     unless @current_user
       respond_with do |format|
         error = {error: "Token not found."}
-        format.json { render json: error }
-        format.xml  { render xml:  error }
+        format.json { render json: error, status: 401 }
+        format.xml  { render xml:  error, status: 401 }
       end
     end
   end
---
+++ spec/controllers/api/feeds_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Api::FeedsController, :type => :api do
+  it "returns feed" do
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    user = Factory.create(:user)
+
+    get :show, host: "api.lvh.me",
+               id: user.display_name,
+               token: user.authentication_token,
+               format: "json"
+
+    response.header['Content-Type'].should include 'application/json'
+    response.status.should == 200
+  end
+end
