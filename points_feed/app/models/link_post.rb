871ebaae0eb2b924f4029f688e767144fab256d0
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-05-14 22:40:05 -0400

    Minor refactor for Link post validation message

--- points_feed/app/models/link_post.rb
+++ points_feed/app/models/link_post.rb
@@ -2,7 +2,7 @@ class LinkPost < Post
   validates :content, :length => { :maximum => 2048 }
 
   validate do
-    self.errors[:base] << "Link must be valid" unless self.content.match(/^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix)
+    self.errors[:base] << "Invalid URL; link must be in the format http://www.example.com/" unless self.content.match(/^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix)
   end
 
   def template
