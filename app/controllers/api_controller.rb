160de5f723e11f858a555a16b2a4332f67b8d638
Jan Koszewski <jan.koszewski@gmail.com>
2012-05-15 13:08:32 -0400

    new working api controller

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,21 @@
+class ApiController < ApplicationController
+  before_filter :authenticate
+
+  attr_accessor :current_user
+
+  private
+
+  def construct_link_header(next_url, last_url)
+    "<#{next_url}>; rel=\"next\", <#{last_url}>; rel=\"last\""
+  end
+
+  def authenticate
+    if self.current_user = authenticate_or_request_with_http_basic do |username, password|
+        login(username, password)
+      # User.where(username: username, password: password)
+      end
+    else
+      request_http_basic_authentication
+    end
+  end
+end
