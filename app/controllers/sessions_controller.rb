0e2d6810018e60a7a8a79225a42f1215f0f139f8
Jonan Scheffler <jonanscheffler@gmail.com>
2012-05-15 13:43:06 -0400

    Test new signup.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    @user = User.find_by_email(params[:email]) || User.new
+    if @user.id && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to dashboard_path, notice: "Logged in!"
     else
-      @user = User.new
       flash.now.alert = "Email or password is invalid."
       render "new"
     end
