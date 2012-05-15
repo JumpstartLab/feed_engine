891b4b1628cc4a40e053954649e27c635fbc2ba3
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-05-14 22:59:41 -0400

    allowed for parallel jobz for twitter

--- app/jobs/twitter_job.rb
+++ app/jobs/twitter_job.rb
@@ -2,16 +2,15 @@ class TwitterJob
   @queue = :tweet 
 
   def self.perform(current_user, authentication)
-   Twitter.configure do |config|
-    config.consumer_key = 'ubCmGXxyj0ZQ2guzFXdg'
-    config.consumer_secret = 'ytoc7GZ05NqSgKZqpW0O1PjyCUiEuPrDuuHV0rLKE'
-    config.oauth_token = authentication["token"]
-    config.oauth_token_secret = authentication["secret"]
-  end
+   client = Twitter::Client.new ({
+    :consumer_key => 'ubCmGXxyj0ZQ2guzFXdg',
+    :consumer_secret => 'ytoc7GZ05NqSgKZqpW0O1PjyCUiEuPrDuuHV0rLKE',
+    :oauth_token => authentication["token"],
+    :oauth_token_secret => authentication["secret"]})
   uid = authentication["uid"] 
 
   user = User.find(current_user["id"])
-  Twitter.user_timeline(uid.to_i).reverse.each do |tweet| 
+  client.user_timeline(uid.to_i).reverse.each do |tweet| 
     twitter_item = user.twitter_items.create(:tweet => tweet)
     user.add_stream_item(twitter_item)
   end 
