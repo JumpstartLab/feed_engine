bf94436f21db0f63398b5f93731cbe104bf6cf15
Travis Valentine <travis.valentine@livingsocial.com>
2012-05-15 20:56:58 -0400

    Tests passing, delayed jobs working like a BOSS

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id              :integer         not null, primary key
+#  subscription_id :integer
+#  body            :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  poster_id       :integer
+#
+
 class Tweet < ActiveRecord::Base
   include Postable
   attr_accessible :body, :subscription_id, :created_at
