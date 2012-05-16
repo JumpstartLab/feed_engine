54a44475a2834942bf9641091e1f73f3bffde166
Melanie Gilman <melanie.gilman@gmail.com>
2012-05-16 14:11:02 -0400

    Instagram account link

--- points_feed/config/initializers/omniauth.rb
+++ points_feed/config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'AJ6A08DwICnc7HOC8PPPww', '3onFLtbQIqIZlHh0MzoKIuNcQb2HQR4Z1D0r3C6MHA'
   provider :github, 'a7b4784e6089f033bbc0', '26eebe36d42cad2fe80d0bae93e15b3862ee4996'
+  provider :instagram, '25e04f4e2bda41fea6e629f724e2066f', '31f053ff5d7a427f8ba993d871118541'
 
   Twitter.configure do |config|
     config.consumer_key = "Kw18pAPRPsgtBpUhVI6Q"
