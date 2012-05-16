24970b3a73cde300ef8ed6b076b4926bb81ae740
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-16 15:13:36 -0400

    Request specs for api user show

--- spec/requests/api_requests_spec.rb
+++ spec/requests/api_requests_spec.rb
@@ -11,12 +11,70 @@ describe "API" do
     reset_host
   end
 
-
-
   context "user's show page" do
-    it "returns a 200" do
-      get feed_path(user.display_name)
-      response.code.should == '200'
+    it "has the user's display name" do
+      visit feed_path(user.display_name)
+      page.should have_content(user.display_name)
+    end
+
+    it "has the user's id" do
+      visit feed_path(user.display_name)
+      page.should have_content(user.id)
+    end
+
+    it "has the user's api show link" do
+      visit feed_path(user.display_name)
+      page.should have_content(feed_path(user.display_name))
+    end
+
+    context "items" do
+      it "has number of pages" do
+        visit feed_path(user.display_name)
+        page.should have_content(user.post_page_count)
+      end
+
+      context "if the user has no posts" do
+        it "shows page count of 0" do
+          visit feed_path(user.display_name)
+          page.should_not have_content("pages: 0")
+        end
+
+        it "has none of the item information" do
+          visit feed_path(user.display_name)
+          page.should_not have_content("?page=1")
+        end
+      end
+
+      context "if the user has posts" do
+        let!(:message1) { Fabricate(:message, poster_id: user.id) }
+        let!(:message2) { Fabricate(:message, poster_id: user.id) }
+        let!(:message3) { Fabricate(:message, poster_id: user.id) }
+        let!(:message4) { Fabricate(:message, poster_id: user.id) }
+        it "has the first page" do
+          visit feed_path(user.display_name)
+          page.should have_content("?page=1")
+        end
+
+        it "has the last page" do
+          visit feed_path(user.display_name)
+          page.should have_content("?page=#{user.post_page_count}")
+        end
+
+        it "has the post_type of the most recent post" do
+          visit feed_path(user.display_name)
+          page.should have_content("Message")
+        end
+
+        it "has the link of the most recent post" do
+          visit feed_path(user.display_name)
+          page.should have_content("/items/#{message4.id}")
+        end
+
+        it "does not include information about older posts" do
+          visit feed_path(user.display_name)
+          page.should_not have_content("/items/#{message1.id}")
+        end
+      end
     end
   end 
 end
