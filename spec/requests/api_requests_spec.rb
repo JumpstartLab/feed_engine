11f79f01a9e3d852fe0bc03b3fd90668eab22953
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-16 15:52:39 -0400

    item index and show tests

--- spec/requests/api_requests_spec.rb
+++ spec/requests/api_requests_spec.rb
@@ -67,14 +67,62 @@ describe "API" do
 
         it "has the link of the most recent post" do
           visit feed_path(user.display_name)
-          page.should have_content("/items/#{message4.id}")
+          page.should have_content("/items/#{message4.item.id}")
         end
 
         it "does not include information about older posts" do
           visit feed_path(user.display_name)
-          page.should_not have_content("/items/#{message1.id}")
+          page.should_not have_content("/items/#{message1.item.id}")
         end
       end
     end
   end 
+
+  #Elise's pet - not required per stories
+  context "item index" do
+    context "when the user has items" do
+      let!(:message) { Fabricate(:message, poster_id: user.id) }
+      let!(:link) { Fabricate(:link, poster_id: user.id) }
+
+      it "has the type of items created" do
+        visit feed_item_index_path(user.display_name)
+        page.should have_content("Message")
+      end
+
+      it "lists all items ever created" do
+        visit feed_item_index_path(user.display_name)
+        page.should have_content(message.body)
+      end
+      it "lists all items ever created" do
+        visit feed_item_index_path(user.display_name)
+        page.should have_content(link.url)
+      end
+    end
+  end
+
+  context "item show page" do
+    let!(:message) { Fabricate(:message, poster_id: user.id) }
+    let!(:link) { Fabricate(:link, poster_id: user.id) }
+
+    context "when it is a message" do
+      it "has the type of the item" do
+        visit feed_item_path(:display_name => user.display_name,
+                             :id => message.item.id)
+        page.should have_content(message.body)
+      end
+      it "has the item's link" do
+        visit feed_item_path(:display_name => user.display_name,
+                             :id => message.item.id)
+        page.should have_content("/items/#{message.item.id}")
+      end
+    end
+    
+    context "when it is a link" do
+      it "has the url of the item" do
+        visit feed_item_path(:display_name => user.display_name,
+                             :id => link.item.id)
+        page.should have_content(link.url)
+      end
+    end
+  end
 end
