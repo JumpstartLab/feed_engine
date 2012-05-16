c4d1f238b21ae3f08632cad0818b7299b8d07b6e
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-05-16 16:26:09 -0400

    Twitter

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,17 +8,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     auth.create_twitter_account(uid: data["info"]["uid"],
                                 nickname: data["info"]["nickname"],
                                 image: data["info"]["image"],
-                                initial_status: data["extra"]["raw_info"]["status"]["id_str"])
+                                last_status_id: data["extra"]["raw_info"]["status"]["id_str"])
     
     redirect_to new_authentication_path, :notice => "Twitter account successfully added."
   end
 
   def github
     data = request.env["omniauth.auth"]
-    current_user.authentications.create(provider: data["provider"],
+    auth = current_user.authentications.create(provider: data["provider"],
                                         token: data["credentials"]["token"],
                                         secret: data["credentials"]["secret"])
     
+     
     redirect_to new_authentication_path, :notice => "Github account successfully added."
   end
 end
