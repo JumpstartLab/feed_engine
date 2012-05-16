7a8d507cb336c2a1321de483ef1b2152d846af63
Andrewglass1 <Andrewglass1@gmail.com>
2012-05-16 14:31:33 -0400

    github link works

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -7,9 +7,9 @@ class AuthenticationsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
+    current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'].to_s)
 
-    #Resque.enqueue(TwitterFeeder, current_user.id)
+    Resque.enqueue(TwitterFeeder, current_user.id)
     flash[:notice] = "#{auth['provider'].capitalize} link successful"
     redirect_to user_root_path
   end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, '5zgijb9G1mtxKnhsTe1cvA', 'VLoxR3kyh3zBAASPCtFt2Qc3fN2M5xK8sUiVNa2w'
-  provider :github, '5ab76ea234765e0857f4', '80e7e6359fb9906494cc613cbf497c7de222727f'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
+
