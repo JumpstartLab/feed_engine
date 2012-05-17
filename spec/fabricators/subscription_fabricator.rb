54f0fa1984d60086a02d5be10b2117da5c699c79
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-17 10:16:43 -0400

    Merge

--- spec/fabricators/subscription_fabricator.rb
+++ spec/fabricators/subscription_fabricator.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: subscriptions
+#
+#  id         :integer         not null, primary key
+#  provider   :string(255)
+#  uid        :string(255)
+#  user_name  :string(255)
+#  user_id    :integer
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 Fabricator(:subscription) do
   provider { ["twitter","github"].sample }
   uid      { 11111111111111 }
--- spec/fabricators/tweet_fabricator.rb
+++ spec/fabricators/tweet_fabricator.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id              :integer         not null, primary key
+#  subscription_id :integer
+#  body            :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  poster_id       :integer
+#
+
 Fabricator(:tweet) do
   subscription_id 1
   body { Faker::Lorem.words(5) }
