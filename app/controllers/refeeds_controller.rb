975151ded565e2147d22166de8c8ac2f5630f02c
michael verdi <michael.v.verdi@gmail.com>
2012-05-16 22:59:06 -0400

    initial refeed, still can refeed posts multiple times

---
+++ app/controllers/refeeds_controller.rb
@@ -0,0 +1,19 @@
+class RefeedsController < ApplicationController
+
+  def create
+    post = Post.find(params[:post_id])
+
+    if post.user == current_user
+      flash[:notice] = "Can't refeed your own post"
+      redirect_to :back
+    else
+      original_post = post
+      postable      = post.postable
+
+      postable_copy = postable.dup
+      current_user.posts.create({postable: postable_copy, refeed_id: original_post.id}, validate: false)
+      redirect_to user_path(post.user)
+    end
+  end
+
+end
