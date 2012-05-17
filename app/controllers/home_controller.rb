b0873c749731cd20be10a4f60f7354a043c73759
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-17 16:11:22 -0400

    Collect all user items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 # The controller for the home that is the splash page
 class HomeController < ApplicationController
   def index
+    @posts = User.all.collect { |user| user.items }.flatten(1)
     redirect_to dashboard_path if current_user
   end
 end
