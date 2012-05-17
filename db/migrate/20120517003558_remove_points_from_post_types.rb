9061b9acb3324293d247921ff803f5a0350e5b46
Charles Strahan <charles.c.strahan@gmail.com>
2012-05-17 14:21:20 -0400

    try to fix migrations

--- db/migrate/20120517003558_remove_points_from_post_types.rb
+++ db/migrate/20120517003558_remove_points_from_post_types.rb
@@ -1,6 +1,5 @@
 class RemovePointsFromPostTypes < ActiveRecord::Migration
   def change
-    remove_column :image_posts, :points
     remove_column :text_posts, :points
     remove_column :link_posts, :points
   end
