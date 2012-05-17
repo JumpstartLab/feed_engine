878e30ef89fb004c5aa1876ac082cbe1af7b5999
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-16 21:24:09 -0400

    Add edit action/view

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -7,4 +7,19 @@ class PasswordResetsController < ApplicationController
     user.send_password_reset if user
     redirect_to root_url, :notice => "Email sent with password reset instructions."
   end
+
+  def edit
+    @user = User.find_by_password_reset_token!(params[:id])
+  end
+
+  def update
+    @user = User.find_by_password_reset_token!(params[:id])
+    if @user.password_reset_sent_at < 12.hours.ago
+      redirect_to new_password_reset_path, :alert => "Password reset expired."
+    elsif @user.update_attributes(params[:user])
+      redirect_to root_url, :notice => "Password has been reset."
+    else
+      render :edit
+    end
+  end
 end
