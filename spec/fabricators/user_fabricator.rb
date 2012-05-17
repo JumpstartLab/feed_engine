e783d20f961bc3b5d212bba972c6f98ab24a0a09
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-16 22:18:36 -0400

    Tests

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,13 +2,15 @@
 #
 # Table name: users
 #
-#  id              :integer         not null, primary key
-#  email           :string(255)
-#  password_digest :string(255)
-#  created_at      :datetime        not null
-#  updated_at      :datetime        not null
-#  display_name    :string(255)
-#  api_key         :string(255)
+#  id                     :integer         not null, primary key
+#  email                  :string(255)
+#  password_digest        :string(255)
+#  created_at             :datetime        not null
+#  updated_at             :datetime        not null
+#  display_name           :string(255)
+#  api_key                :string(255)
+#  password_reset_token   :string(255)
+#  password_reset_sent_at :datetime
 #
 
 Fabricator(:user) do
