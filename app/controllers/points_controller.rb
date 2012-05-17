8e49152aa02f2e7869b398054ce8c3f55e6dc9f3
michael verdi <michael.v.verdi@gmail.com>
2012-05-16 23:35:50 -0400

    fixed point path for redirecting

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -2,6 +2,6 @@ class PointsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update_attribute(:points, @post.add_point)
-    redirect_to dashboard_path
+    redirect_to user_path(@post.user.id)
   end
 end
