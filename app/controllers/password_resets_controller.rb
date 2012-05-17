ce2f3ecfd2e594788f83ce096aa44b20527b26a1
Travis Valentine <travis.valentine@livingsocial.com>
2012-05-17 16:23:26 -0400

    bug fixes

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,6 +17,7 @@ class PasswordResetsController < ApplicationController
     if @user.password_reset_sent_at < 12.hours.ago
       redirect_to new_password_reset_path, :alert => "Password reset expired."
     elsif @user.update_attributes(params[:user])
+      session[:user_id] = @user.id
       redirect_to root_url, :notice => "Password has been reset."
     else
       render :edit
