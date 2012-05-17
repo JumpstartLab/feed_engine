dd501219120e892a74096856c9ec61eba6fdde66
Travis Valentine <travis.valentine@livingsocial.com>
2012-05-16 22:13:46 -0400

    add ribbons to feed items

--- db/schema.rb
+++ db/schema.rb
@@ -37,22 +37,6 @@ ActiveRecord::Schema.define(:version => 20120516002105) do
     t.string   "repo"
   end
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
-
   create_table "images", :force => true do |t|
     t.text     "description"
     t.text     "url"
