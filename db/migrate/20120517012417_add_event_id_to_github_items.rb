27e95b56930a9e2ad72f141cf2e204d40c6dd1f6
Austen Ito <austen.services@gmail.com>
2012-05-17 09:00:21 -0400

    Fixed the db migrations for events

--- db/migrate/20120517012417_add_event_id_to_github_items.rb
+++ db/migrate/20120517012417_add_event_id_to_github_items.rb
@@ -1,7 +1,6 @@
 class AddEventIdToGithubItems < ActiveRecord::Migration
   def change
     add_column :github_items, :event_id, :string
-    add_column :github_items, :event, :text
     remove_column :github_items, :gist
   end
 end
