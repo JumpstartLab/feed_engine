0df06b13bd26b84b90f2c8ef3abcba9da955c680
Jan Koszewski <jan.koszewski@gmail.com>
2012-05-16 20:04:44 -0400

    working json text posts from the api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,14 +1,17 @@
 class ApiController < ApplicationController
-  before_filter :authenticate
-
+  before_filter :authenticate_user, only: [:create, :edit, :destroy]
   attr_accessor :current_user
 
   private
 
-  def authenticate
-    @current_user = User.find_by_authentication_token(params[:token])
+  def authenticate_user
+    @current_user = User.find_by_authentication_token(auth_token)
     unless @current_user
-      respond_with({:error => "Token is invalid." })
+      head status: :unauthorized
     end
+  end
+
+  def auth_token
+    request.env['HTTP_TOKEN']
   end
 end
