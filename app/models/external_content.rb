7aa2ef87889c238d48a782e0779961883923ab1b
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-17 10:54:08 -0400

    Fixed validation problems for external content description

--- app/models/external_content.rb
+++ app/models/external_content.rb
@@ -3,7 +3,9 @@ module ExternalContent
     target.class_eval do
       validates_presence_of :poster_id
       validates_length_of :url, in: 1..2048
-      validates_length_of :description, in: 1..256
+      validates :description,
+                :length => { :maximum => 256 },
+                :allow_blank => true
       validates_format_of( :url,
                    with: /^((http|https?):\/\/((?:[-a-z0-9]+\.)+[a-z]{2,}))/,
                    message: "Invalid link format",
