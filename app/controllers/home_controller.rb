cc490db20657fab0479e13ee31b7a7d8a6051c33
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-17 16:47:04 -0400

    Add pagination - 20 posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 # The controller for the home that is the splash page
 class HomeController < ApplicationController
   def index
-    @posts = Item.all_items_sorted_posts
-    # redirect_to dashboard_path if current_user
+    @posts = Kaminari.paginate_array(
+               Item.all_items_sorted_posts
+             ).page(params[:page]).per(20)
   end
 end
