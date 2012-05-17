2895d2205640a4952a298d68ebf55fb2ee41d2f0
Chris Anderson <christopher.anderson@gmail.com>
2012-05-17 17:12:15 -0400

    added error validation on incorrect url

--- app/models/link_item.rb
+++ app/models/link_item.rb
@@ -1,11 +1,11 @@
 class LinkItem < ActiveRecord::Base
   include Streamable
-
+  LINK_REGEX = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
   attr_accessible :comment, :url
 
   validates_presence_of :url
   validates_length_of :url, :maximum => 2048
-  validates_format_of :url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  validates_format_of :url, :with => LINK_REGEX, :message => "Please enter a valid url" 
   validates_length_of :comment, :maximum => 256
 
   has_many :stream_items, :as => :streamable
