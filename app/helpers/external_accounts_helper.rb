e6f62041193f94b48051c4838c6be0c20f035119
Austen Ito <austen.services@gmail.com>
2012-05-16 21:58:10 -0400

    Added connect and disconnect buttons to external accounts page

--- app/helpers/external_accounts_helper.rb
+++ app/helpers/external_accounts_helper.rb
@@ -1,2 +1,9 @@
 module ExternalAccountsHelper
+  def twitter_connected?
+    Authentication.where("user_id = ? and provider = ?", current_user.id, "twitter").any?
+  end
+
+  def github_connected?
+    Authentication.where("user_id = ? and provider = ?", current_user.id, "github").any?
+  end
 end
