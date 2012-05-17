dc75cf2f244aac20f9be6e64af011a8863fc7740
Jan Koszewski <jan.koszewski@gmail.com>
2012-05-17 14:12:29 -0400

    refeeding from api

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,5 +4,5 @@ class Link < ActiveRecord::Base
   validates :comment, length: { maximum: 256, message: "The comment must be no longer than 256 characters." }
   validates_format_of :content, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
   validates :content, length: { maximum: 2048 }, presence: true
-  has_one :post, :as => :postable
+  has_many :posts, :as => :postable
 end
