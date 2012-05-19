0e02d97eb293637b836bc742cea3d9bea9d0cece
michael verdi <michael.v.verdi@gmail.com>
2012-05-18 22:09:45 -0400

    twitter works, but no go on github still

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -7,8 +7,14 @@ class AuthenticationsController < ApplicationController
     omniauth = request.env["omniauth.auth"]
     auth = current_user.authentications.find_or_initialize_by_provider(omniauth["provider"])
     if auth.create_with_omniauth(omniauth)
-      redirect_to dashboard_url(subdomain: false),
-        notice: "#{omniauth["provider"].capitalize} account connected!"
+      if omniauth["provider"] == "twitter"
+        redirect_to github_sign_in_page
+      elsif omniauth["provider"] == "github"
+        redirect_to instagram_sign_in_page
+      elsif omniauth["provider"] == "instagram"
+        redirect_to dashboard_path
+      end
+      flash[:notice] = "#{omniauth["provider"].capitalize} account connected!"
     else
       redirect_to dashboard_url(subdomain: false),
         notice: "Something went wrong connecting to #{omniauth["provider"].capitalize}! Please try again!"
@@ -16,7 +22,7 @@ class AuthenticationsController < ApplicationController
   end
 
   def skip
-    redirect_to dashboard_url(subdomain: false), 
+    redirect_to dashboard_url(subdomain: false),
       notice: "You can link your account at any time by clicking on the buttons to the right!"
   end
 
@@ -25,5 +31,15 @@ class AuthenticationsController < ApplicationController
     current_user.remove_auth_for(provider_name)
     flash[:notice] = "Successfully Destroyed"
     redirect_to dashboard_path
+  end
+
+  private
+
+  def github_sign_in_page
+    "/user_signup_steps/github"
+  end
+
+  def instagram_sign_in_page
+    "/user_signup_steps/instagram"
   end
 end
