06947553513486a8b12dbb508a6074df2cd45ebc
Chris Anderson <christopher.anderson@gmail.com>
2012-05-19 01:39:39 -0400

    initial tests of secret feature complete

--- app/controllers/text_items_controller.rb
+++ app/controllers/text_items_controller.rb
@@ -7,6 +7,9 @@ class TextItemsController < ApplicationController
   def create
     @text_item = current_user.text_items.new(params[:text_item])
     if @text_item.save
+      Pusher['test_channel'].trigger('greet', {
+  :greeting => "#{@text_item.body}" 
+})
       redirect_to dashboard_path, notice: 'Post was successfully created.'
     else
       @link_item = LinkItem.new
