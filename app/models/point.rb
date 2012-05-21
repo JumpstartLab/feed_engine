94d6c06da136828794b0bf15e26d9de3769e90a0
Chris Anderson <christopher.anderson@gmail.com>
2012-05-21 18:12:24 -0400

    point can't be added by user that owns post

--- app/models/point.rb
+++ app/models/point.rb
@@ -6,4 +6,11 @@ class Point < ActiveRecord::Base
 
   validates_presence_of :pointable_id, :user_id
   validates_uniqueness_of :pointable_id, :scope => [:user_id, :pointable_type]
+  validate :user_id_and_pointable
+
+  def user_id_and_pointable
+    unless pointable == nil
+      errors.add(:password, "You can't point your own post") if user_id == pointable.user_id
+    end
+  end
 end
--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Point do
   let(:user)       { FactoryGirl.create(:user) }
+  let(:user_2)    { FactoryGirl.create(:user) }
   let(:image_item) { FactoryGirl.create(:image_item) }
 
   it "requires both a user and stream item" do
@@ -9,6 +10,12 @@ describe Point do
     z = Point.new(pointable: image_item)
     p.should_not be_valid
     z.should_not be_valid
+  end
+
+  it "doesn't let a user point their own post" do
+    text_item = FactoryGirl.create(:text_item, user: user)
+    p = Point.new(user: user, pointable: text_item)
+    p.should_not be_valid
   end
 
   it "saves with a user and stream item" do
