67a386ee79fc490e98b8c44d32715136552a99a3
Mike Chlipala <tom.kiefhaber+mikesea@hungrymachine.com>
2012-05-21 00:06:06 -0400

    random commit, not sure why floating minidash quit working, getting to the bottom of it now

---
+++ app/assets/javascripts/dashboard_tabs.js
@@ -0,0 +1,7 @@
+
+
+$(function() {
+    $( "#content_tabs" ).tabs({
+      event: "mouseover"
+    });
+  });
--- app/assets/javascripts/scrolling_minidash.js
+++ app/assets/javascripts/scrolling_minidash.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).ready(function(){
   $(window).scroll(function () { 
     $('#minidash').css('top', $(document).scrollTop()); 
   });
