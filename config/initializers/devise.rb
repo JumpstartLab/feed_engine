ac9a271ac49117813ac2761e571ba9db9fac04f1
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-05-21 17:09:35 -0400

    no more switching keys, created env conditions

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -211,11 +211,13 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-    # config.omniauth :twitter, ENV["TWITTER_DEV_KEY"], ENV["TWITTER_DEV_SECRET"], :scope => "users"
-    config.omniauth :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"], :scope => "users"
-    # config.omniauth :github, ENV["GITHUB_DEV_KEY"], ENV["GITHUB_DEV_SECRET"], :scope => "users"
-    config.omniauth :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], :scope => "users"
-
+    if Rails.env.production? 
+      config.omniauth :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], :scope => "users"
+      config.omniauth :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"], :scope => "users"
+    else
+      config.omniauth :twitter, ENV["TWITTER_DEV_KEY"], ENV["TWITTER_DEV_SECRET"], :scope => "users"
+      config.omniauth :github, ENV["GITHUB_DEV_KEY"], ENV["GITHUB_DEV_SECRET"], :scope => "users"
+    end
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
