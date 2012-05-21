45e67cd3f9e7c1e811818ce6140b342571eaf37b
athal7 <andrew.thal@livingocial.com>
2012-05-21 14:54:07 -0400

    make notice work for pending points login from controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,10 +6,8 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     if current_user
       try_to_add_point
-      redirect_to session[:return_to]
     else
-      session[:point_pending_for] = @item.id
-      redirect_to login_url(:subdomain => false)
+      prompt_login_with_pending_point
     end
   end
 
@@ -18,6 +16,17 @@ class ItemsController < ApplicationController
   def try_to_add_point
     if params[:points] && @item.poster_id != current_user.id
       @item.post.increase_point_count
+      redirect_to session[:return_to]
+    else
+      notice = "Points! were not added, an unknown error occurred"
+      redirect_to session[:return_to], notice: notice
     end
+  end
+
+  def prompt_login_with_pending_point
+    session[:point_pending_for] = @item.id
+    notice = "You must be logged in to give Points! " +
+      "<a href='#{session[:return_to]}'>No thanks, take me back</a>".html_safe
+    redirect_to login_url(:subdomain => false), notice: notice
   end
 end
