0ff3139710f8f0524fd349fdded04373b7bcaad4
Chris Anderson <christopher.anderson@gmail.com>
2012-05-21 17:30:53 -0400

    changed mailer callback url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ FeedEngine::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'polutropos.com' }
+  config.action_mailer.default_url_options = { :host => 'troutr.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
