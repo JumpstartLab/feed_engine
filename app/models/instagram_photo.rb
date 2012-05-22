61791f9e2b8f5f4e7dd7f03543596409e60d4266
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-05-22 11:24:48 -0400

    instagram photo upload now pulls only the lastest & the actual photo

--- app/models/instagram_photo.rb
+++ app/models/instagram_photo.rb
@@ -1,5 +1,14 @@
 class InstagramPhoto < Growl
   belongs_to :user
+  after_validation :send_photo_to_amazon
+
+  def send_photo_to_amazon
+    begin
+      self.photo = open(link)
+    rescue
+      #errors.add(:link, "Photo does not exist")
+    end
+  end
 
   def icon
     "glyphicons/glyphicons_400_instagram.png"
--- spec/factories/meta_data.rb
+++ spec/factories/meta_data.rb
@@ -1,5 +1,3 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :meta_datum, :class => 'MetaData' do
     title "Pandas"
