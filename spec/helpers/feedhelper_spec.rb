60c0035b27999b344c33f34beb12aef3a479a808
Chris Anderson <christopher.anderson@gmail.com>
2012-05-22 01:53:50 -0400

    modded feedhelper spec

--- spec/helpers/feedhelper_spec.rb
+++ spec/helpers/feedhelper_spec.rb
@@ -13,6 +13,8 @@ describe FeedHelper do
     @item = double("item")
     @item.stub_chain(:event, :actor, :login).and_return("Chris")
     @item.stub_chain(:event, :payload, :commits).and_return("commits")
+    @item.stub_chain(:event, :repo, :name).and_return("commits")
+    @item.stub_chain(:event, :payload, :ref_type).and_return("commits")
     @item.stub_chain(:event, :payload, :forkee, :html_url).and_return("http://google.com")
   end
 
@@ -21,23 +23,23 @@ describe FeedHelper do
   describe "#github_body" do
     it "processess a Push event" do
       @item.stub_chain(:event, :type).and_return("PushEvent")
-      @dummy.github_body(@item)
-      @dummy.instance_variable_get(:@github_body).should == "Chris pushed the following commits"
+      @dummy.github_body(@item).should == "Chris pushed the following commits:"
       @dummy.instance_variable_get(:@commits).should == "commits"
     end
 
     it "processess a Fork event" do
       @item.stub_chain(:event, :type).and_return("ForkEvent")
-      @dummy.github_body(@item)
-      @dummy.instance_variable_get(:@fork_url).should == "http://google.com"
-      @dummy.instance_variable_get(:@github_body).should == "Chris forked"
+      @dummy.github_body(@item).should include "Forked"
+    end
+
+    it "processess a created event" do
+      @item.stub_chain(:event, :type).and_return("CreateEvent")
+      @dummy.github_body(@item).should include "Created"
     end
 
     it "processes a undefined event" do
       @item.stub_chain(:event, :type).and_return("random")
-      @dummy.github_body(@item)
-      @dummy.instance_variable_get(:@github_body).should == "Chris performed a random"
-      @dummy.instance_variable_get(:@event_type).should == "github_event_unsupported"
+      @dummy.github_body(@item).should include "performed"
     end
   end
 end
