e59cd9c22db21450d01596db230c509eea495e83
Charles Strahan <charles.c.strahan@gmail.com>
2012-05-22 17:14:32 -0400

    Fix simplecov

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,8 +4,8 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
-require 'cucumber/rails'
 require 'simplecov'
+require 'cucumber/rails'
 
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
