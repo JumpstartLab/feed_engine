b1b9c49f1f2bed2314c46675817a33190c148694
Melanie Gilman <melanie.gilman@gmail.com>
2012-05-22 17:54:58 -0400

    More user decorator tests

--- points_feed/app/controllers/friends_controller.rb
+++ points_feed/app/controllers/friends_controller.rb
@@ -4,6 +4,6 @@ class FriendsController < ApplicationController
   def destroy
     friendship = current_user.friendships.where(:friend_id => params[:id]).first
     friendship.destroy if friendship
-    redirect_to dashboard_path, :notice => "That friend did suck"
+    redirect_to dashboard_path, :notice => "That user has been removed"
   end
 end
