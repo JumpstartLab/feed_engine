d53d22de067ad159a769d7c41ed7525d8983d3b4
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-22 13:27:21 -0400

    LOLOLOLOL Blog -> FeedEngine

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.feed_engine.sock"
 worker_processes 2
 timeout 30
