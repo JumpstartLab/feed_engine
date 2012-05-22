e19a239139ec098e162bdcd4e0e8fcc9ef45e160
Edward Weng <edweng@gmail.com>
2012-05-22 17:31:04 -0400

    Making more changes to the instagram processor

--- spec/lib/instagram_processor_spec.rb
+++ spec/lib/instagram_processor_spec.rb
@@ -6,16 +6,21 @@ describe Hungrlr::InstagramProcessor do
   context ".run" do
     it "create instagram photos for a collection of users" do
       account = double
-      account.stub(:[]).with("nickname").and_return("nick")
+      account.stub(:[]).with("instagram_id").and_return("instagram_id")
+      account.stub(:[]).with("token").and_return("token")
       account.stub(:[]).with("last_status_id").and_return("status_id")
       account.stub(:[]).with("user_id").and_return("user_id")
-      
-      Net::HTTP.should_receive(:get)
-      JSON.should_receive(:parse).and_return( { 'accounts' => [account] } )
+      api_service = double
+      subject.stub(:api_service => api_service)
 
-      subject.should_receive(:get_tweets).with("nick", "status_id").and_return([])
-      subject.should_receive(:build_tweet_hash).with([]).and_return([])
-      subject.should_receive(:create_tweets_for_user).with("user_id", [])
+      subject.should_receive(:instagram_accounts).and_return([ account ])
+
+      data = double
+      Time.should_receive(:parse).and_return( "" )
+      Net::HTTP.should_receive(:get).and_return(data)
+      JSON.should_receive(:parse).with(data).and_return( { 'data' => [] } )
+      api_service.should_receive(:build_photos_hash).with([]).and_return([])
+      api_service.should_receive(:create_photos_for_user).with("user_id", [])
 
       subject.run
     end
