d9ec4040340dc3371089e1dfa02d0e10be760701
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-22 00:02:03 -0400

    small deploy change

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:eifion/#{application}.git"
+set :repository, "git@github.com:eliseworthy/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
