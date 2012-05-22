97a27d2b2a508ed8da779ecaae1419aec4d8f4cf
Horace Williams <horace.d.williams@gmail.com>
2012-05-21 21:19:57 -0400

    SWIM IN THE GLORIOUS RIVER

--- app/controllers/river_controller.rb
+++ app/controllers/river_controller.rb
@@ -1,8 +1,8 @@
 class RiverController < ApplicationController
   def show
-    @stream_items = StreamItem.order("created_at DESC").limit(5)
+    @stream_items = StreamItem.where(refeed: false).order("created_at DESC").limit(5)
     @items = @stream_items.collect { |i| i.streamable }
+    current_user
     render :template => "devise/sessions/new"
-
   end
 end
