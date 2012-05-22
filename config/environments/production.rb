cd225e25b72c5f53dbc50150d059bb561c92de01
Mike Chlipala <tom.kiefhaber+mikesea@hungrymachine.com>
2012-05-21 22:41:18 -0400

    revised production settings for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,14 @@ FeedEngine::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => 'feedonkulous.com' }
+  ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
 end
