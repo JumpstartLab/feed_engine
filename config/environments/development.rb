4b2d0c347869067becd6068c1fdaf62b110aaa9f
Elise Worthy <elise.worthy@livingsocial.com>
2012-05-22 19:46:04 -0400

    set up redis to go

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 FeedEngine::Application.configure do
+  ENV["REDISTOGO_URL"] = 'redis://localhost:6379'
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
