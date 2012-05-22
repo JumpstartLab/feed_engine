4bada9de3ee93ab26981b6b52d27ca7317f160b1
Chris Anderson <christopher.anderson@gmail.com>
2012-05-22 10:53:59 -0400

    implemented point cache

--- db/migrate/20120510001222_create_image_items.rb
+++ db/migrate/20120510001222_create_image_items.rb
@@ -4,6 +4,7 @@ class CreateImageItems < ActiveRecord::Migration
       t.string :url
       t.text :comment
       t.references :user
+      t.integer  :points_count, :default => 0
       t.timestamps
     end
   end
--- db/migrate/20120515011427_create_twitter_items.rb
+++ db/migrate/20120515011427_create_twitter_items.rb
@@ -3,7 +3,7 @@ class CreateTwitterItems < ActiveRecord::Migration
     create_table :twitter_items do |t|
       t.text :tweet
       t.references :user
-
+      t.integer  :points_count, :default => 0
       t.timestamps
     end
     add_index :twitter_items, :user_id
