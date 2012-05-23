ed148d0312dd088c2bf7e4649068810579f74abb
Chris Anderson <christopher.anderson@gmail.com>
2012-05-23 12:05:16 -0400

    RIVER ALL THE THINGS

--- app/controllers/text_items_controller.rb
+++ app/controllers/text_items_controller.rb
@@ -6,9 +6,15 @@ class TextItemsController < ApplicationController
     @text_item = current_user.text_items.new(params[:text_item])
     if @text_item.save
       @new_stream_item = current_user.stream_items.find(@text_item.to_param)
-      Pusher['new_channel'].trigger('hello', { :html => render_to_string(:partial => "feed/item", :locals => { :item => @new_stream_item }),
+      x = render_to_string(:partial => "feed/item", :locals => { :item => @new_stream_item }, :layout => false )
+      Pusher['new_channel'].trigger('hello', { :html => x,
         :stream_id => @new_stream_item.id } 
         )
+      respond_to do |format|
+        format.js { render :create }
+      end
+
+
     end
     @link_item = LinkItem.new
     @image_item = ImageItem.new
