a79f350887979bd484e8e60bef4e5fba4593476f
Chris Anderson <christopher.anderson@gmail.com>
2012-05-22 21:24:56 -0400

    Major refactor of view layer, hopefully preserving ajax in dashboard

--- app/controllers/link_items_controller.rb
+++ app/controllers/link_items_controller.rb
@@ -3,9 +3,11 @@ class LinkItemsController < ApplicationController
   before_filter :create_feed_items, :only => [:create]
 
   def create
+    @link_item = current_user.link_items.new(params[:link_item])
+    if @link_item.save
+      @new_stream_item = current_user.stream_items.find(@link_item.to_param)
+    end
     @text_item = TextItem.new
     @image_item = ImageItem.new
-    @link_item = current_user.link_items.new(params[:link_item])
-    @link_item.save
   end
 end
