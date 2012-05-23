9d234187b58c885605e7189036fe3c9a12779db2
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-05-23 18:54:23 -0400

    fixing videos tab

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,4 +1,5 @@
 class AuthenticationsController < ApplicationController
+  before_filter :authenticate_user!
   def new
   end
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
     redirect_to dashboard_path, :flash => flash if current_user
-    @growls = Growl.order(:created_at).page(params[:page])
+    @growls = Growl.order(:original_created_at).page(params[:page])
   end
 end
