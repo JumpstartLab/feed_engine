5b6811d66797eb68a4f889945b53f665dd3efd96
Chris Anderson <christopher.anderson@gmail.com>
2012-05-23 10:54:30 -0400

    THE RIVER. IT FLOWS.

--- app/controllers/text_items_controller.rb
+++ app/controllers/text_items_controller.rb
@@ -5,9 +5,10 @@ class TextItemsController < ApplicationController
   def create
     @text_item = current_user.text_items.new(params[:text_item])
     if @text_item.save
-      x = render_to_string(@text_item)
-      Pusher['new_channel'].trigger('hello', {:author => x })
       @new_stream_item = current_user.stream_items.find(@text_item.to_param)
+      Pusher['new_channel'].trigger('hello', { :html => render_to_string(:partial => "feed/item", :locals => { :item => @new_stream_item }),
+        :stream_id => @new_stream_item.id } 
+        )
     end
     @link_item = LinkItem.new
     @image_item = ImageItem.new
