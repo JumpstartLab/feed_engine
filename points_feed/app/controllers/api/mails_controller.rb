6c5c98be4b9748a3a1bfa8cfe0ffa9438758738f
Nisarg Shah <nisargshah100@gmail.com>
2012-05-23 15:46:39 -0400

    mailing

--- points_feed/app/controllers/api/mails_controller.rb
+++ points_feed/app/controllers/api/mails_controller.rb
@@ -1,7 +1,18 @@
 class Api::MailsController < ApplicationController
 
-  def show
-    raise params.inspect
+  def create
+    subject = Sanitize::clean(params["subject"])
+    body = Sanitize::clean(params["body-html"])
+    sender = params["sender"]
+
+    user = User.where(:email => sender).first
+
+    unless user.blank?
+      user.text_posts.create(:content => subject + " " + body)
+      render :json => true
+    else
+      error(403)
+    end
   end
 
 end
