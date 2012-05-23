448d101c95849bc55bda00336895e9a1cd596cc3
Horace Williams <horace.d.williams@gmail.com>
2012-05-23 16:25:30 -0400

    user find bug

--- app/controllers/api/stream_items_controller.rb
+++ app/controllers/api/stream_items_controller.rb
@@ -1,7 +1,7 @@
 class Api::StreamItemsController < Api::BaseController
   before_filter :verify_auth_token_match, :only => :create
   def show
-    @user = User.find_by_display_name(:display_name => params[:display_name])
+    @user = User.find_by_display_name(params[:display_name])
     @stream_item = @user.stream_items.find(params[:id])
     @item = @stream_item.streamable
   end
