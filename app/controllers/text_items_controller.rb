24965e2547ddbc5be6c64b7fdb1a51fa61ca1a6d
Chris Anderson <christopher.anderson@gmail.com>
2012-05-23 10:06:18 -0400

    testing river

--- app/controllers/text_items_controller.rb
+++ app/controllers/text_items_controller.rb
@@ -5,6 +5,8 @@ class TextItemsController < ApplicationController
   def create
     @text_item = current_user.text_items.new(params[:text_item])
     if @text_item.save
+      x = render_to_string(@text_item)
+      Pusher['new_channel'].trigger('hello', {:author => x })
       @new_stream_item = current_user.stream_items.find(@text_item.to_param)
     end
     @link_item = LinkItem.new
