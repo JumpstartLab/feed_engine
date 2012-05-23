05c8a150de4c748d191c898a3eaadb4a840a09c7
Nisarg Shah <nisargshah100@gmail.com>
2012-05-23 19:38:41 -0400

    uploads

--- points_feed/config/initializers/carrierwave.rb
+++ points_feed/config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS'       # required
-    # :aws_access_key_id      => 'xxx',       # required
-    # :aws_secret_access_key  => 'yyy',       # required
+    :provider               => 'AWS',       # required
+    :aws_access_key_id      => ENV['aws_access_key_id'],       # required
+    :aws_secret_access_key  => ENV['aws_secret_access_key']      # required
     # :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'pointsfeedin'                     # required
