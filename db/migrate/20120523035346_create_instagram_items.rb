a315f9768a384920efad5bac09a05062f3bd2857
Chris Anderson <christopher.anderson@gmail.com>
2012-05-23 17:02:18 -0400

    Fixed instagram migration

--- db/migrate/20120523035346_create_instagram_items.rb
+++ db/migrate/20120523035346_create_instagram_items.rb
@@ -3,7 +3,7 @@ class CreateInstagramItems < ActiveRecord::Migration
     create_table :instagram_items do |t|
       t.text :image
       t.string :image_id
-
+      t.integer  :points_count, :default => 0
       t.timestamps
     end
   end
