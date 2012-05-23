a39051fb993e928ae4db9fb9f6128e3fba9cb58e
Horace Williams <horace.d.williams@gmail.com>
2012-05-23 00:06:12 -0400

    providing wrapper method for accessing tweet text in views

--- app/models/twitter_item.rb
+++ app/models/twitter_item.rb
@@ -6,6 +6,10 @@ class TwitterItem < ActiveRecord::Base
 
   serialize :tweet
 
+  def tweet_text
+    tweet["text"]
+  end
+
   def self.create_from_json(user_id, parsed_json)
     new(:user_id => user_id,
         status_id: parsed_json["tweet"]["id_str"],
@@ -13,25 +17,3 @@ class TwitterItem < ActiveRecord::Base
         tweet: parsed_json["tweet"])
   end
 end
-
-
-# Tweet:
-#   Status;
-#   Attrs
-#     created_at
-#     id
-#     id_str
-#     text
-#     source
-#     truncated
-#     in_reply_to
-#     in_rep...
-#     etc...
-#
-#     TimeStamp: twitter_item.tweet.created_at
-#
-#     TWEETJSON
-#     {"status_id":"#{item.tweet.attrs["id_str"]}", "attrs":"#{t.tweet.attrs}"}
-#
-#     item.tweet.class == Twitter::Status
-#     item.tweet.attrs.class == Hash --> parse this to json
