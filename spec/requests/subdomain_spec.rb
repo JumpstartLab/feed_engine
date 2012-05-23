d91345f22fd916b0b017754fac789c6741668640
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-05-23 16:33:10 -0400

    fixing tests

--- spec/requests/subdomain_spec.rb
+++ spec/requests/subdomain_spec.rb
@@ -37,17 +37,9 @@ describe "Subdomains" do
     it "shows the subdomain user's most recent messages" do
       visit root_path
       page.should have_content "#{user.display_name.capitalize}'s Feed"
-      user.growls.each do |growl|
+      user.growls.take(5).each do |growl|
         page.should have_content growl.comment if growl.comment.present?
         page.should have_content growl.link if growl.link?
-      end
-    end
-
-    describe "logged in user visits another user's subdomain" do
-      before do
-        login(user)
-        Capybara.app_host = "http://#{second_user.display_name}.hungry.test"
-        visit root_path
       end
     end
   end
