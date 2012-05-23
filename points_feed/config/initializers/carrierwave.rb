984bb880f732db6d999738680516e49b3f3a19a6
Nisarg Shah <nisargshah100@gmail.com>
2012-05-23 19:30:50 -0400

    carrierwave bucket

--- points_feed/config/initializers/carrierwave.rb
+++ points_feed/config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     # :aws_secret_access_key  => 'yyy',       # required
     # :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'pointsfeed'                     # required
+  config.fog_directory  = 'pointsfeedin'                     # required
   # config.fog_host       = 'https://s3.amazonaws.com'            # optional, defaults to nil
   config.fog_public     = true                                   # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
