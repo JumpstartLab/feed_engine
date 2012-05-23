1892c8f0cb731a397c56b3082148d7317b9d4802
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-05-23 12:00:40 -0400

    posting instagram photos to the dashboard/river at long last

--- app/models/instagram_item.rb
+++ app/models/instagram_item.rb
@@ -8,6 +8,14 @@ class InstagramItem < ActiveRecord::Base
 
   serialize :image
 
+  def image_url
+    image["images"]["standard_resolution"]["url"]
+  end
+
+  def caption
+    image["caption"]["text"]
+  end
+
   def self.create_from_json(user_id, parsed_json)
     new(user_id: user_id,
         image_id: parsed_json["image"]["id"],
