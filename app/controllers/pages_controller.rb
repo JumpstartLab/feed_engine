99130aa6ca2654b9d93370f1deccdf285ff21b9c
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-05-24 09:09:04 -0400

    style changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def home
     redirect_to dashboard_path, :flash => flash if current_user
     @growls = Growl.by_date.page(params[:page])
+    render :layout => "layouts/home"
   end
 end
