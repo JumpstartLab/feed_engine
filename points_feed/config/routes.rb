e9539e2ae59313d9eabb9fec0519935460d7e2a5
Nisarg Shah <nisargshah100@gmail.com>
2012-05-23 23:08:04 -0400

    background moved to a queue

--- points_feed/config/routes.rb
+++ points_feed/config/routes.rb
@@ -1,4 +1,7 @@
+require 'resque/server'
+
 PointsFeed::Application.routes.draw do
+  mount Resque::Server.new, :at => "/resque"
   resources :authentications
 
   match "home/index" => redirect("/")
