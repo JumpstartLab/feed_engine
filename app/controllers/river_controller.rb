01a746e73a0a25949039bde957818bed8b6eb13f
Chris Anderson <christopher.anderson@gmail.com>
2012-05-23 22:03:21 -0400

    speed changes

--- app/controllers/river_controller.rb
+++ app/controllers/river_controller.rb
@@ -1,6 +1,6 @@
 class RiverController < ApplicationController
   def show
-    @stream_items = StreamItem.where(refeed: false).order("created_at DESC").limit(5)
+    @stream_items = StreamItem.includes(:user, :streamable).where(refeed: false).order("created_at DESC").limit(5)
     current_user
     render :template => "devise/sessions/new"
   end
