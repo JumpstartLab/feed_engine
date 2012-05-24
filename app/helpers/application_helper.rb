736b3f014aec7d75633c8255a581c11e4cd1b1be
Austen Ito <austen.services@gmail.com>
2012-05-23 20:06:34 -0400

    Changed Stream to My Stream. Changed the linked services to hooked services

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,18 +4,26 @@ module ApplicationHelper
   end
 
   def github_connected?(user)
-    Authentication.where("user_id = ? and provider = ?", user.id, "github").any?
+    service_connected(user, "github")
   end
 
   def twitter_connected?(user)
-    Authentication.where("user_id = ? and provider = ?", user.id, "twitter").any?
+    service_connected(user, "twitter")
+  end
+
+  def instagram_connected?(user)
+    service_connected(user, "instagram")
+  end
+
+  def service_connected(user, service)
+    Authentication.where("user_id = ? and provider = ?", user.id, service).any?
   end
 
   def linked_services?(user)
     if user.nil?
       false
     else
-      twitter_connected?(user) || github_connected?(user)
+      twitter_connected?(user) || github_connected?(user) || instagram_connected?(user)
     end
   end
 end
