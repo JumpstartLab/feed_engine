45916487a80d85ee737c186f8229985604748dc6
Chris Anderson <christopher.anderson@gmail.com>
2012-05-23 23:22:12 -0400

    Implemented some fragment cachine

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ FeedEngine::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # mailer configs
   config.action_mailer.default_url_options = { :host => 'feedengine.dev' }
