9a0c8cea697490c80959116761cae6093307f0b8
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-05-24 09:01:11 -0400

    new spawns instance variable

--- app/controllers/river_controller.rb
+++ app/controllers/river_controller.rb
@@ -1,8 +1,8 @@
 class RiverController < ApplicationController
   def show
     @stream_items = StreamItem.where(refeed: false).order("created_at DESC").limit(5)
-    @new_spawns = User.new_spawns
     current_user
+    @new_spawns = User.new_spawns
     render :template => "devise/sessions/new"
   end
 end
