f53b23559a036ee78e134cdcf671277704dcb6b4
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-05-24 10:01:24 -0400

    swapped icon buttons

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
   def twitter_button
     if current_user.provider_added?("twitter")
-      link_to image_tag("/assets/twitter_logo_lnkd.gif"), 
+      link_to image_tag("/assets/twitter_logo.gif"), 
       authentication_url("twitter"), 
       method: "delete", id: "unlink_twitter"
     else
-      link_to image_tag("/assets/twitter_logo.gif"), 
+      link_to image_tag("/assets/twitter_logo_lnkme.gif"), 
       "/auth/twitter", 
       method: "post", class: "link_twitter"
     end
@@ -13,11 +13,11 @@ module UsersHelper
 
   def github_button
     if current_user.provider_added?("github")
-      link_to image_tag("/assets/github_logo_lnkd.gif"), 
+      link_to image_tag("/assets/github_logo.gif"), 
       authentication_url("github"), 
       method: "delete", class: "unlink_github"
     else
-      link_to image_tag("/assets/github_logo.gif"), 
+      link_to image_tag("/assets/github_logo_lnkme.gif"), 
       "/auth/github", 
       method: "post", class: "link_github"
     end
@@ -25,11 +25,11 @@ module UsersHelper
 
   def instagram_button
     if current_user.provider_added?("instagram")
-      link_to image_tag("/assets/instagram_logo_lnkd.gif"), 
+      link_to image_tag("/assets/instagram_logo.gif"), 
       authentication_url("instagram"), 
       method: "delete", class: "unlink_instagram"
     else
-      link_to image_tag("/assets/instagram_logo.gif"), 
+      link_to image_tag("/assets/instagram_logo_lnkme.gif"), 
       "/auth/instagram", 
       method: "post", class: "link_instagram"
     end
