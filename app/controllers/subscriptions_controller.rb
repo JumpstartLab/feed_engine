aac94c03183a08a6f82847d979374e6f9bc570bf
Chris Anderson <christopher.anderson@gmail.com>
2012-05-23 21:50:44 -0400

    Notification on new follower

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,6 +2,7 @@ class SubscriptionsController < ApplicationController
   def create
     followed_user = User.find(params[:followed_user_id])
     current_user.subscriptions.create(:followed_user_id => params[:followed_user_id])
+    Pusher["#{followed_user.display_name}"].trigger('refeed', {:new_follower => current_user.display_name, :followee => followed_user.display_name } )
     redirect_to :back, :notice => "Added #{followed_user.display_name} to your TackleBox!"
   end
 
