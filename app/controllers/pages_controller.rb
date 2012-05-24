1a9986db7c2ec98b1a4354fc90d73f3d5a9630eb
Edward Weng <edweng@gmail.com>
2012-05-23 21:29:49 -0400

    Fixing ordering of growls on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
     redirect_to dashboard_path, :flash => flash if current_user
-    @growls = Growl.order(:original_created_at).page(params[:page])
+    @growls = Growl.by_date.page(params[:page])
   end
 end
