b580a81db7f427108e3309b7c527a82ac8549b2e
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-05-25 08:52:59 -0400

    fixing refeed processor

--- app/lib/refeed_processor.rb
+++ app/lib/refeed_processor.rb
@@ -5,55 +5,37 @@ require "./config/initializers/twitter"
 
 module Hungrlr
   class RefeedProcessor
+    attr_accessor :base_url, :bj_token
 
     def initialize
-    end
-
-    def bj_token
-      ENV["BJ_TOKEN"].present? ? ENV["BJ_TOKEN"] : "HUNGRLR"
-    end
-
-    def base_url
-      ENV["DOMAIN"].present? ? ENV["DOMAIN"] : "http://api.lvh.me:3000/v1"
+      self.bj_token = ENV["BJ_TOKEN"].present? ? ENV["BJ_TOKEN"] : "HUNGRLR"
+      self.base_url = ENV["DOMAIN"].present? ? ENV["DOMAIN"] : "http://api.hungrlr.dev/v1"
     end
 
     def subscriptions
-      url = "#{base_url}/subscriptions.json?token=#{bj_token}"
-      subscriptions = Net::HTTP.get(URI(url))
+      subscriptions = Net::HTTP.get(URI("#{base_url}/subscriptions.json?token=#{bj_token}"))
       JSON.parse(subscriptions)["subscriptions"]
     end
 
     def get_growls(user_slug, last_status_id)
-      url = "#{base_url}/feeds/#{user_slug}/growls.json?token=#{bj_token}&since=#{last_status_id + 1}"
-      growls = Net::HTTP.get(URI(url))
+      growls = Net::HTTP.get(URI("#{base_url}/feeds/#{user_slug}/growls.json?token=#{bj_token}&since=#{last_status_id}"))
       JSON.parse(growls)["growls"]
     end
 
     def create_regrowls_for(subscription, growls)
+      user_slug = subscription["user_slug"]
+      subscriber_token = subscription["subscriber_token"]
+      id = subscription["id"]
+
       growls.each do |growl|
-        url = "#{base_url}/feeds/#{user_slug(subscription)}/growls/#{growl["id"]}/refeed"
-        uri_path = URI(url)
-        Net::HTTP.post_form(uri_path, token: subscriber_token(subscription),
-                            subscription_id: id(subscription))
+        uri_path = URI("#{base_url}/feeds/#{user_slug}/growls/#{growl["id"]}/refeed")
+        Net::HTTP.post_form(uri_path, token: subscriber_token, subscription_id: id)
       end
-    end
-
-    def user_slug(subscription)
-      subscription["user_slug"]
-    end
-
-    def subscriber_token(subscription)
-      subscription["subscriber_token"]
-    end
-
-    def id(subscription)
-      subscription["id"]
     end
 
     def run
       subscriptions.each do |subscription|
-        growls = get_growls(subscription["user_slug"],
-                            subscription["last_status_id"])
+        growls = get_growls(subscription["user_slug"], subscription["last_status_id"])
         create_regrowls_for(subscription, growls)
       end
     end
